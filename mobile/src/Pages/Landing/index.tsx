4d27d0519edf29ad0796606c1293fbf00ed54386
DavysonMelo <mdavyson@gmail.com>
2020-08-06 15:34:45 -0300

    Proffy's mobile app

---
+++ mobile/src/Pages/Landing/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png';
+import studdyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+
+function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClasses() {
+    navigate('GiveClasses');
+  }
+
+  function handleNavigateToStudy() {
+    navigate('Study');
+  }
+
+  return(
+    <View style={styles.container}>
+      <Image style={styles.banner} source={landingImg}/>
+
+      <Text style={styles.title} >
+        Seja bem vindo, {'\n'}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonContainer}>
+        <RectButton
+          style={[styles.button, styles.buttonPrimary]}
+          onPress={handleNavigateToStudy}
+        >
+          <Image source={studdyIcon}/>
+          <Text style={styles.buttonText}>Estudar</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={handleNavigateToGiveClasses}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClassesIcon}/>
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de 200 conexões realizadas {' '}
+        <Image source={heartIcon}/>
+      </Text>
+    </View>
+  )
+}
+
+export default Landing;
+
